d8551d1a75d6a633f4cb9a5cb23874bdff2515aa
Aaryan0484 <aaryankumarak18@gmail.com>
2023-07-24 23:45:42 +0530

    Weather-app

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getweather(city: string, units: string) {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ce9fcb00c1841412ae20c3fe5b116d2f&units=metric');
+  }
+}
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,50 @@
+import { WeatherService } from 'src/app/services/weather.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+  myWeather: any;
+  temperature: number = 0;
+  feelsLikeTemp: number = 0;
+  humidity: number = 0;
+  pressure: number = 0;
+  summary: string = '';
+  iconURL: string = '';
+  city: string = 'Sundernagar';
+  units: string = 'imperial';
+  cityName: string = ''; // Add this line to declare the cityName variable
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    //this.getWeather();
+  }
+
+  onSubmit() {
+    this.getWeather(this.city, this.units); // Provide both city and units
+  }
+
+  private getWeather(city: string, units: string) { // Add 'units' parameter here
+    this.weatherService.getweather(city, units).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.myWeather = res;
+        this.temperature = this.convertTemperature();
+        this.feelsLikeTemp = this.convertTemperature();
+        this.humidity = this.myWeather.main.humidity;
+        this.pressure = this.myWeather.main.pressure;
+        this.summary = this.myWeather.weather[0].main;
+
+        this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
+      }
+    });
+  }
+
+  convertTemperature(): number {
+    return parseFloat(((this.temperature - 32) * 5 / 9).toFixed(2));
+  }
+}
